14d478c994d7986a39dd94d36d54d75047e9d06b
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-01 18:16:39 -0300

    feat: add prompt

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import prompt from "prompt";
+
+import mainPrompt from "./prompts/prompt-main.js";
+
+async function main() {
+    prompt.get(mainPrompt, async (err, result) => {
+      if (choose.select = 1) console.log("escolheu o qrdCODE");
+      if (choose.select = 2) console.log("escolheu o password");
+    });
+    prompt.start();
+}
+
+main();

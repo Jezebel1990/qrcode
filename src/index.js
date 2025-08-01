10521f15c11cde43a5c30ed9eae6523348b7bbeb
Jezebel1990 <jezebelguedes@gmail.com>
2025-08-01 18:52:25 -0300

    feat: add Chalk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import prompt from "prompt";
 import mainPrompt from "./prompts/prompt-main.js";
 
 async function main() {
-    prompt.get(mainPrompt, async (err, result) => {
+    prompt.get(mainPrompt, async (err, choose) => {
       if (choose.select = 1) console.log("escolheu o qrdCODE");
       if (choose.select = 2) console.log("escolheu o password");
     });
